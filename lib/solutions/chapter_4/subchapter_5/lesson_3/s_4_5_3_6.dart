ee1296489a1caee7f9d52f5a31280d96de2014d7
Dopa91 <code.dopatka@gmail.com>
2024-09-23 10:47:21 +0200

    task 4.5.3.6 done

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -4,6 +4,35 @@ class S4536 extends StatelessWidget {
   const S4536({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Row(
+            children: [
+              Text(products[index].name),
+              Text(products[index].price.toString()),
+            ],
+          ),
+          leading: const Icon(Icons.shopping_cart),
+        );
+      },
+    );
   }
 }
+
+class Product {
+  final String name;
+  final double price;
+
+  Product({required this.name, required this.price});
+}
+
+final List<Product> products = [
+  Product(name: "Gamecard ", price: 29.99),
+  Product(name: "Headphones ", price: 129.99),
+  Product(name: "iPhone ", price: 829.99),
+  Product(name: "iMac ", price: 1229.99),
+  Product(name: "Sweets ", price: 9.99),
+];
